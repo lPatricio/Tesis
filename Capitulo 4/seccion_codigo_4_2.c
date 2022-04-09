b68856dd3732d2627a22af0a20626b9b1ff25c8c
Luis Patricio <lpatricio-lopez@hotmail.com>
2022-04-09 01:25:28 -0300

    Add files via upload

---
+++ Capitulo 4/seccion_codigo_4_2.c
@@ -0,0 +1,11 @@
+void liberarMemoriaDinamicaMatriz(double*** A ){
+		//Descripcion:libera memoria dinamica de un puntero triple utilizado como matriz
+		//Entrada:A,puntero triple de tipo double
+		//Salida: 
+
+		int i;
+		for(i=0;i<FILASMATRIZ;i++){
+			free( (*A)[i]);
+		}
+				
+}	
